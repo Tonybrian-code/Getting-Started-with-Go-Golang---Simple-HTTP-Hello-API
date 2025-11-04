7e7f35b3b7d66c3773ced15b03007d0738381ce5
Tonybrian Ochieng Otieno <deluxetony14@gmail.com>
2025-11-04 05:05:46 +0000

    done

--- go-hello/main.go
+++ go-hello/main.go
@@ -1,17 +1,24 @@
-
 package main
 
 import (
     "fmt"
+    "math/rand"
     "net/http"
+    "time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-   // fmt.Fprintf(w, "Hello, World! Welcome to Go Web Server 🚀")
+    jokes := []string{
+        "Why do Gophers love Go? Because it makes concurrency easy!",
+        "I told my code a joke... it didn’t catch it!",
+        "Go programmers never panic… oh wait.",
+    }
+    rand.Seed(time.Now().UnixNano())
+    fmt.Fprintf(w, "%s", jokes[rand.Intn(len(jokes))])
 }
 
 func main() {
     http.HandleFunc("/", handler)
-    fmt.Println("Server starting on http://localhost:8080")
+    fmt.Println("Joke API running at http://localhost:8080")
     http.ListenAndServe(":8080", nil)
 }
